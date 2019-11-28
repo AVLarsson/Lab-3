23316c602da7313666f8e688eb9b205290fbd311
Emma Andersson <emmamariagaby@gmail.com>
2019-11-28 11:22:10 +0100

    Co-authored-by: boggzer <jonathan.bogg@gmail.com> Co-authored-by: Annie  <annlar2@hotmail.com>

--- build/build.js
+++ build/build.js
@@ -2,9 +2,11 @@
 
 window.onload = function() {
     printDate();
+    renderTime();
 }
 
 let date = new Date();
+
 function printDate() {
     date.setDate(1);
     let day = date.getDay();
@@ -12,7 +14,7 @@ function printDate() {
     let currentMonth = date.getMonth()
     let currentYear = date.getFullYear()
     let endDate = new Date(currentYear, currentMonth + 1, 0).getDate();
-    
+
 
     let previousDate = new Date(currentYear, currentMonth, 0).getDate();
     const months = [
@@ -52,4 +54,45 @@ function moveDate(para) {
         date.setMonth(date.getMonth() + 1);
     }
     printDate();
+}
+
+function renderTime() {
+
+    // Date
+    let myDate = new Date();
+    let year = myDate.getFullYear();
+    if (year < 1000) {
+        year += 1900
+    }
+    let day = myDate.getDay();
+    let month = myDate.getMonth();
+    let daym = myDate.getDate();
+    let dayArray = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+    let montArray = new Array("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12");
+
+    // Time
+    let currentTime = new Date();
+    let h = currentTime.getHours();
+    let m = currentTime.getMinutes();
+    let s = currentTime.getSeconds();
+    if (h === 24) {
+        h = 0;
+    } else if (h > 12) {
+        h = h - 0;
+    }
+    if (h < 10) {
+        h = "0" + h;
+    }
+    if (m < 10) {
+        m = "0" + m;
+    }
+    if (s < 10) {
+        s = "0" + s;
+    }
+
+    let myClock = document.getElementById("clockDisplay");
+    myClock.textContent = "" + dayArray[day] + " " + daym + "/" + montArray[month] + " " + year + " " + h + ":" + m + ":" + s;
+    myClock.innerText = "" + dayArray[day] + " " + daym + "/" + montArray[month] + " " + year + " " + h + ":" + m + ":" + s;
+
+    setTimeout("renderTime()", 1000);
 }
