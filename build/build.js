5accc64002e0118ed04e624be8f81cbbf277f49b
j.bogg <jonathan.bogg@gmail.com>
2019-12-03 12:25:34 +0100

    updated working calendar

--- build/build.js
+++ build/build.js
@@ -2,19 +2,24 @@
 
 window.onload = function() {
     printDate();
-    renderTime();
 }
 
 let date = new Date();
 
 function printDate() {
-    let day = date.getDay();
-    day = day
+    date.setDate(7);
+    console.log(date);
     
+    let day = date.getDay();
     let today = new Date();
+
+    console.log(today);
+    
     let currentMonth = date.getMonth()
     let currentYear = date.getFullYear()
-    let endDate = new Date(currentYear, currentMonth, 0).getDate();
+    let endDate = new Date(currentYear, currentMonth + 1, 0).getDate();
+    
+
     let previousDate = new Date(currentYear, currentMonth, 0).getDate();
     const months = [
         "January",
@@ -31,13 +36,12 @@ function printDate() {
         "December"
     ];
 
-
     document.getElementById("month").innerHTML = months[currentMonth] + " " + currentYear;
     /*document.getElementsByClassName("date").innerHTML = date.toDateString();*/
     let cells = "";
     for (let number = day; number > 0; number--) {
-         cells += "<div class='previous_date'>" + (previousDate - number) + "</div>";
-     }
+        cells += "<div class='previous_date'>" + (previousDate - number + 1) + "</div>";
+    }
     for (let i = 1; i <= endDate; i++) {
         if (i == today.getDate() && currentMonth == today.getMonth()) cells += "<div class='today'>" + i + "</div>";
         else
@@ -56,42 +60,3 @@ function moveDate(para) {
     printDate();
 }
 
-function renderTime() {
-
-    // Date
-    let myDate = new Date();
-    // if (year < 1000) {
-        // year += 1900
-    // }
-    let day = myDate.getDay();
-    let month = myDate.getMonth();
-    let daym = myDate.getDate();
-    let dayArray = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
-    let montArray = new Array("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12");
-
-    // Time
-    let currentTime = new Date();
-    let h = currentTime.getHours();
-    let m = currentTime.getMinutes();
-    let s = currentTime.getSeconds();
-    if (h === 24) {
-        h = 0;
-    } else if (h > 12) {
-        h = h - 0;
-    }
-    if (h < 10) {
-        h = "0" + h;
-    }
-    if (m < 10) {
-        m = "0" + m;
-    }
-    if (s < 10) {
-        s = "0" + s;
-    }
-
-    let myClock = document.getElementById("clockDisplay");
-    myClock.textContent = "" + dayArray[day] + " " + daym + "/" + montArray[month] + " " + " " + h + ":" + m + ":" + s;
-    myClock.innerText = "" + dayArray[day] + " " + daym + "/" + montArray[month] + " " + " " + h + ":" + m + ":" + s;
-
-    setTimeout("renderTime()", 1000);
-}
