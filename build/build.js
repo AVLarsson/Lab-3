c8176f086624dd76203845126266690e0c15444d
j.bogg <jonathan.bogg@gmail.com>
2019-12-03 12:07:12 +0100

    update

--- build/build.js
+++ build/build.js
@@ -8,14 +8,13 @@ window.onload = function() {
 let date = new Date();
 
 function printDate() {
-    date.setDate(1);
     let day = date.getDay();
+    day = day
+    
     let today = new Date();
     let currentMonth = date.getMonth()
     let currentYear = date.getFullYear()
-    let endDate = new Date(currentYear, currentMonth + 1, 0).getDate();
-
-
+    let endDate = new Date(currentYear, currentMonth, 0).getDate();
     let previousDate = new Date(currentYear, currentMonth, 0).getDate();
     const months = [
         "January",
@@ -32,12 +31,13 @@ function printDate() {
         "December"
     ];
 
+
     document.getElementById("month").innerHTML = months[currentMonth] + " " + currentYear;
     /*document.getElementsByClassName("date").innerHTML = date.toDateString();*/
     let cells = "";
     for (let number = day; number > 0; number--) {
-        cells += "<div class='previous_date'>" + (previousDate - number + 1) + "</div>";
-    }
+         cells += "<div class='previous_date'>" + (previousDate - number) + "</div>";
+     }
     for (let i = 1; i <= endDate; i++) {
         if (i == today.getDate() && currentMonth == today.getMonth()) cells += "<div class='today'>" + i + "</div>";
         else
