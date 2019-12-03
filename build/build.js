b034c1c80127464669e8f8f37bf27db330f0742d
j.bogg <jonathan.bogg@gmail.com>
2019-12-03 12:27:26 +0100

    -

--- build/build.js
+++ build/build.js
@@ -2,6 +2,7 @@
 
 window.onload = function() {
     printDate();
+    renderTime();
 }
 
 let date = new Date();
@@ -60,3 +61,42 @@ function moveDate(para) {
     printDate();
 }
 
+function renderTime() {
+
+    // Date
+    let myDate = new Date();
+    // if (year < 1000) {
+        // year += 1900
+    // }
+    let day = myDate.getDay();
+    let month = myDate.getMonth();
+    let daym = myDate.getDate();
+    let dayArray = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+    let montArray = new Array("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12");
+
+    // Time
+    let currentTime = new Date();
+    let h = currentTime.getHours();
+    let m = currentTime.getMinutes();
+    let s = currentTime.getSeconds();
+    if (h === 24) {
+        h = 0;
+    } else if (h > 12) {
+        h = h - 0;
+    }
+    if (h < 10) {
+        h = "0" + h;
+    }
+    if (m < 10) {
+        m = "0" + m;
+    }
+    if (s < 10) {
+        s = "0" + s;
+    }
+
+    let myClock = document.getElementById("clockDisplay");
+    myClock.textContent = "" + dayArray[day] + " " + daym + "/" + montArray[month] + " " + " " + h + ":" + m + ":" + s;
+    myClock.innerText = "" + dayArray[day] + " " + daym + "/" + montArray[month] + " " + " " + h + ":" + m + ":" + s;
+
+    setTimeout("renderTime()", 1000);
+} 
