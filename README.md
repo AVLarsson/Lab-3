e7d7aaa40e24f98475ffd744dffc074d6f672f84
j.bogg <jonathan.bogg@gmail.com>
2019-12-06 15:41:52 +0100

    file removal, to do count, to do list
    removed unneccessary/empty files, added to do count on each day, added a list of all to dos

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Lab-3
+# Lab -3
+#### Grupp: Annie, Emma, Jonathan
+
+## Kalender
+### Fungerande kalender
+Kalendervyn visar månad för månad och börjar vid dagens år och månad. Den markerar också dagens datum. Det är lätt att byta månad genom att klicka på vardera pil om rubriken med månad och år.
+
+### Att göra-lista
+Användaren kan själv lägga till "att göra-punkter"/"to dos" genom att ange datum och fri text. Användaren har en komplett översikt över alla punkter till vänster. I kalendern visas alla punkter i rätt dag samt med en siffra för att hålla koll på hur många punkter det finns för den dagen.
+
+### Dagens tid och datum
+Uppe ligger ett block som visar både dagens (rörliga) tid och datum.
+
---
+++ js/build.js
@@ -0,0 +1,218 @@
+"use strict";
+
+/* On load, print days, show clock and add event listener to add button */
+window.addEventListener('load', () => {
+    printDate();
+    renderTime();
+    document.getElementById('add').addEventListener('click', addToDo);
+})
+
+// Global variable shared and used by printDate() and moveDate().
+// Needs to be worked into/communicated between each function.
+let date = new Date();
+
+/** Print content for each day */
+function printDate() {
+    printToDos();
+    date.setDate(7);
+    let day = date.getDay();
+    let today = new Date();
+    let currentMonth = date.getMonth();
+    let currentYear = date.getFullYear();
+    let endDate = new Date(currentYear, currentMonth + 1, 0).getDate();
+    let previousDate = new Date(currentYear, currentMonth, 0).getDate();
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+    document.getElementById("month").innerHTML = months[currentMonth] + " " + currentYear;
+    let cells = "";
+    for (let number = day; number > 0; number--) {
+        cells += "<div class='previous_date'>" + (previousDate - number + 1) + "</div>";
+    }
+    for (let i = 1; i <= endDate; i++) {
+        let dayContent = dayNumber(i, currentMonth, currentYear);
+
+
+        if (i == today.getDate() && currentMonth == today.getMonth()) {
+            cells += "<div class='today'>" + dayContent + "</div>"
+        }
+        else {
+            cells += "<div>" + dayContent + "</div>";
+        }
+        document.getElementsByClassName('days')[0].innerHTML = cells;
+        let removeButton = document.getElementsByClassName('remove');
+        for (let iTwo = 0; iTwo < removeButton.length; iTwo++) {
+            removeButton[iTwo].addEventListener('click', removeToDo);
+        }
+    }
+}
+
+/**
+ * Create content for each day
+ * @param {number} calDate 
+ * @param {number} calMonth 
+ * @param {number} calYear 
+ * @returns {[string | number]} dateContent
+ */
+function dayNumber(calDate, calMonth, calYear) {
+    const allToDos = getToDoList();
+    let dateContent = [];
+    calMonth = calMonth + 1;
+    // Check if to dos exist
+    if (allToDos.length) {
+        allToDos.forEach(function (toDo) {
+            const newDate = new Date(toDo[1]);
+            let toDoDate = newDate.getDate();
+            let toDoMonth = newDate.getMonth();
+            toDoMonth += 1;
+            let toDoYear = newDate.getFullYear();
+
+            if (calYear = toDoYear) {
+                if (calMonth === toDoMonth) {
+                    if (calDate === toDoDate) {
+
+                        // Create HTML element, add content and remove button
+                        let removeButton = document.createElement('button')
+                        removeButton.classList.add('remove');
+                        removeButton.setAttribute('id', allToDos.indexOf(toDo));
+                        removeButton.innerHTML = 'x';
+                        let div = document.createElement('div');
+                        div.classList.add('savedtodo');
+                        div.innerHTML = toDo[0];
+                        div.appendChild(removeButton);
+                        dateContent.push(div.outerHTML)
+                        dateContent.join(" ");
+                    }
+                }
+            }
+        })
+
+        // Add date number and number of tasks
+        if (0 < dateContent.length) {
+            dateContent.unshift("<span class='task_number'>" + dateContent.length + " task(s)</span>");
+        }
+        dateContent.unshift("<span class='date_number'>" + calDate + "</span>");
+    }
+    // Check if to dos don't exist
+    else if (!allToDos.length) {
+        dateContent.push(calDate);
+    }
+    // Return content to print to array
+    return dateContent.join(" ");
+}
+
+/**
+ * Change month in calendar view
+ * @param {string} button 
+ */
+function moveDate(button) {
+    if (button == 'previous') {
+        date.setMonth(date.getMonth() - 1);
+    }
+    else if (button == 'next') {
+        date.setMonth(date.getMonth() + 1);
+    }
+    printDate();
+}
+
+/**
+ * Recieve saved to dos as array
+ * @returns {string[]} allToDos
+ */
+function getToDoList() {
+    let allToDos = [];
+    let toDoContent = localStorage.getItem('todo');
+    if (toDoContent !== null) {
+        allToDos = JSON.parse(toDoContent);
+    }
+    event.preventDefault();
+    return allToDos;
+}
+
+/**
+ * Save user input as to do
+ * @returns {boolean} false
+ */
+function addToDo() {
+    let toDo = [];
+    let task = document.getElementById('inputText').value;
+    let date = document.getElementById('inputDate').value;
+
+    // Check if input fields are empty
+    if (task == (null || undefined || "") ||
+        date == (null || undefined || "")) {
+            showWarning();
+    }
+    else if (date && task) {
+        let allToDos = getToDoList();
+        toDo.push(task, date);
+        allToDos.push(toDo);
+        localStorage.setItem('todo', JSON.stringify(allToDos));
+    }
+
+    printDate();
+
+    return false;
+}
+
+/** Replace button text to warning if no user input */
+function showWarning() {
+    const button = document.getElementById('add');
+    let originalText = button.innerText;
+    button.innerText = "Vänligen fyll i alla fält korrekt.";
+    button.classList.add('red');
+    setTimeout(function () {
+        button.innerText = originalText;
+        button.classList.remove('red');
+    }, 1500);
+}
+
+/**
+ * Remove to do 
+ * @returns {boolean} false
+ */
+function removeToDo() {
+    let id = event.currentTarget.getAttribute('id');
+    let allToDos = getToDoList();
+    allToDos.splice(id, 1);
+    localStorage.setItem('todo', JSON.stringify(allToDos));
+    printDate();
+    return false;
+}
+
+function printToDos() {
+    let allToDos = getToDoList();
+    let listElement = document.querySelector('.alltodos');
+    let ul = document.createElement('ul');
+    allToDos.forEach(function (toDo) {
+        let li = document.createElement('li');
+        li.append(toDo[1] + ": " + toDo[0])
+        ul.append(li);
+    });
+    listElement.innerHTML = "";
+    listElement.append(ul);
+}
+
+/**
+ * (skriv vad den gör här, t ex Show calendar)
+ */
+function myFunction() {
+    let x = document.getElementById("showCalendar");
+    if (x.classList.contains("showCalendar")) {
+        x.classList.remove("showCalendar");
+    }
+    else {
+        x.classList.add("showCalendar");
+    }
+}
